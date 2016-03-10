bb45d8fb1b5021b4a3ea86180c2014d1fc28df31
szymonzawodny <zawodny.szymon@gmail.com>
2016-03-10 16:43:26 +0100

    ukonczony scroll button

---
+++ js/scrollButton.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+
+
+    $("#scrollButton").click(function () {
+        console.log('clicked!');
+        $('body').animate({
+            scrollTop: 0
+
+        }, 1000);
+
+        return false;
+    });
+
+    $(window).on('load scroll', function(event) {
+        var height = $(window).scrollTop();
+        if(height > 400) {
+            $("#scrollButton").show(500);
+        }
+
+        else{
+            $("#scrollButton").hide(500);
+        }
+
+    });
+
+});
