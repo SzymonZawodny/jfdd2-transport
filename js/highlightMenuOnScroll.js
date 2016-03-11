717edf7d592366bf01613ea2a658e55d14eddd48
mateuszRybczonek <m.rybczonek@gmail.com>
2016-03-11 15:16:17 +0100

    changing nav-items class when scrolling down

--- js/highlightMenuOnScroll.js
+++ js/highlightMenuOnScroll.js
@@ -1,10 +1,33 @@
 $(document).ready(function(){
     $(window).on('load scroll', function() {
         var positionY = $(window).scrollTop();
-        if(positionY > 400) {
-            $('.header, .nav-item')
-                .css({'border-bottom-color': '#FFF'
-                     ,'border-top-color':'#FFF'});
+        if(positionY > 0) {
+            $('.header .nav-item').eq(2)
+                .addClass('nav-item-white');
+        }
+        else{
+            $('.header .nav-item').eq(2)
+                .removeClass('nav-item-white');
+        }
+        if(positionY > 700) {
+            $('.header .nav-item').eq(1)
+                .addClass('nav-item-white');
+            $('.header .nav-item').eq(2)
+                .removeClass('nav-item-white');
+        }
+        else{
+            $('.header .nav-item').eq(1)
+                .removeClass('nav-item-white');
+        }
+        if(positionY > 2650) {
+            $('.header .nav-item').eq(0)
+                .addClass('.nav-item-white');
+            $('.header .nav-item').eq(1)
+                .removeClass('nav-item-white');
+        }
+        else{
+            $('.header .nav-item').eq(0)
+                .removeClass('nav-item-white');
         }
     });
 });
