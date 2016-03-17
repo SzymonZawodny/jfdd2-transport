70632466c2ef8663a733f67eb4dd910bbf16fc79
mateuszRybczonek <m.rybczonek@gmail.com>
2016-03-17 16:15:20 +0100

    - initial function animating ONE bus to wait random time --> approach --> stop at the bus stop for a random period of time --> depart

--- game/js/createTableWithRandomBuses.js
+++ game/js/createTableWithRandomBuses.js
@@ -5,6 +5,7 @@ var $gameboard=$('.gameboard');
 $(document).ready(function (){
   createArrayWithRandomBuses(100);
   createMultipleBusesAndRemoveThemFromArray(5);
+  busApproach();
 });
 
 function createArrayWithRandomBuses(howManyBusesInBuffer){
@@ -26,8 +27,38 @@ function createMultipleBusesAndRemoveThemFromArray(numberOfBuses){
   for (arrayItem=0;arrayItem<numberOfBuses;arrayItem+=1){
     var divClass=busesBuffer[0];
     busesBuffer.splice(0,1);
-    var $bus = $('<div>').addClass(divClass).addClass('bus')
+    var $bus = $('<div>')
+      .addClass(divClass)
+      .addClass('bus')
+      .addClass('bus'+arrayItem)
       .css({top:100*(arrayItem+1), left:'50px'});
     $gameboard.append($bus);
   }
+}
+
+function busApproach(){
+  var initialMargin=0;
+  var distanceTarget=200;
+  var distanceDone=0;
+  var speed=10;
+  var approachTime=(distanceTarget+initialMargin)*speed;
+  var stopTime=(Math.random()*3)*1000+500;
+
+  var intervalBusApproach = setInterval(function(){
+    $('.bus1').css('margin-left','+=1px');
+    distanceDone+=1;
+    if(distanceDone===initialMargin+distanceTarget){
+      clearInterval(intervalBusApproach);
+    }
+  },speed);
+
+  setTimeout(function(){
+    var intervalBusDeparture = setInterval(function(){
+      $('.bus1').css('margin-left','+=1px');
+      distanceDone+=1;
+      if(distanceDone===700){
+        clearInterval(intervalBusDeparture);
+      }
+    },speed);
+  },approachTime+stopTime);
 }
