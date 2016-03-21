025127b1e9c335b4846cf22bb05e59121b91da55
mateuszRybczonek <m.rybczonek@gmail.com>
2016-03-21 16:37:32 +0100

    - animation stops (all intervals and timeout) on game over;

--- game/js/busesGame.js
+++ game/js/busesGame.js
@@ -1,65 +1,64 @@
-var busesBuffer=[];
+var busesBuffer = [];
 var busClass;
-var $buses=$('.buses');
-var howManyBuses, intervalNumber=0;
-var score=0, time;
+var $buses = $('.buses');
+var howManyBuses, intervalNumber = 0;
+var score = 0, time;
 var list = [];
-var intervalsAndTimeouts=[];
+var ids = [];
 
-$(document).ready(function (){
+$(document).ready(function () {
   createArrayWithRandomBuses(100);
   createSpansWithBuses(howManyBuses);
-  counter(5);
-  busAnimation(0,450);
-  busAnimation(1,700);
-  busAnimation(2,550);
-  busAnimation(3,730);
-  busAnimation(4,340);
-  busAnimation(5,540);
-  var busAnimationInterval =  setInterval(function(){
-    intervalsAndTimeouts.push(busAnimationInterval);
-    busAnimation(6+6*intervalNumber,450);
-    busAnimation(7+6*intervalNumber,700);
-    busAnimation(8+6*intervalNumber,550);
-    busAnimation(9+6*intervalNumber,730);
-    busAnimation(10+6*intervalNumber,340);
-    busAnimation(11+6*intervalNumber,540);
-    intervalNumber+=1;
-  },7000);
+  counter(59);
+  busAnimation(0, 450);
+  busAnimation(1, 700);
+  busAnimation(2, 550);
+  busAnimation(3, 730);
+  busAnimation(4, 340);
+  busAnimation(5, 540);
+  ids.push(setInterval(function () {
+    busAnimation(6 + 6 * intervalNumber, 450);
+    busAnimation(7 + 6 * intervalNumber, 700);
+    busAnimation(8 + 6 * intervalNumber, 550);
+    busAnimation(9 + 6 * intervalNumber, 730);
+    busAnimation(10 + 6 * intervalNumber, 340);
+    busAnimation(11 + 6 * intervalNumber, 540);
+    intervalNumber += 1;
+  }, 7000));
 });
 
-  function counter(howManySeconds) {
-    for (var numberOfSeconds = howManySeconds; numberOfSeconds > -1; numberOfSeconds -=1) {
-      list.push(numberOfSeconds);
-    }
-    for (var x in list) {
-      setTimeout(function (a){
-        time=howManySeconds-a;
-        $('.remainingTime').html(time);
-        if (time===0){
-          intervalsAndTimeouts.forEach(function(index){
-            clearInterval(index);
-            clearTimeout(index);
-            intervalsAndTimeouts=[];
-          });
-        }
-      },1000*x,x);
-    }
+function counter(howManySeconds) {
+  for (var numberOfSeconds = howManySeconds; numberOfSeconds > -1; numberOfSeconds -= 1) {
+    list.push(numberOfSeconds);
   }
+  for (var x in list) {
+    ids.push(setTimeout(function (a) {
+      time = howManySeconds - a;
+      $('.remainingTime').html(time);
+      if (time === 0) {
+        ids.forEach(function (index) {
+          clearInterval(index);
+          clearTimeout(index);
+        });
+        ids = [];
+      }
+    }, 1000 * x, x));
+  }
+}
 
-function createArrayWithRandomBuses(howManyBusesInBuffer){
-  for (var busNo = 0; busNo<howManyBusesInBuffer; busNo+=1){
-    var number=Math.random();
-    if(number<0.5){
-      busClass='busToOBC';
+function createArrayWithRandomBuses(howManyBusesInBuffer) {
+  for (var busNo = 0; busNo < howManyBusesInBuffer; busNo += 1) {
+    var number = Math.random();
+    if (number < 0.5) {
+      busClass = 'busToOBC';
       busesBuffer.push(busClass);
     }
-    else{
-      busClass='busToNowhere';
+    else {
+      busClass = 'busToNowhere';
       busesBuffer.push(busClass);
     }
   }
-  howManyBuses=howManyBusesInBuffer;
+  howManyBuses = howManyBusesInBuffer;
 }
 
 function createSpansWithBuses(numberOfBuses) {
@@ -76,52 +75,49 @@ function createSpansWithBuses(numberOfBuses) {
   }
 }
 
-function busAnimation(indexInTheBuffer,busStopDistance){
-  var initialMargin=0;
-  var distanceTarget=busStopDistance;
-  var distanceDone=0;
-  var speed=16;
-  var approachTime=(distanceTarget+initialMargin)*speed/2;
-  var stopTime=2000;
-  var delayTime=(Math.random()*2)*1000;
+function busAnimation(indexInTheBuffer, busStopDistance) {
+  var initialMargin = 0;
+  var distanceTarget = busStopDistance;
+  var distanceDone = 0;
+  var speed = 16;
+  var approachTime = (distanceTarget + initialMargin) * speed / 2;
+  var stopTime = 2000;
+  var delayTime = (Math.random() * 2) * 1000;
 
-  var timeOutBusApproach = setTimeout(function(){ //ARRIVAL
-    intervalsAndTimeouts.push(timeOutBusApproach);///////////////////////////////
-    var intervalBusApproach = setInterval(function(){
-      intervalsAndTimeouts.push(intervalBusApproach);////////////////////////////
-    $('.bus').eq(indexInTheBuffer).css('margin-left','+=2px');
-    distanceDone+=2;
-    if(distanceDone===initialMargin+distanceTarget){
-      clearInterval(intervalBusApproach);
-    }
-  },speed);
-  },delayTime);
-
-  var timeOutBusStop = setTimeout(function(){
-    intervalsAndTimeouts.push(timeOutBusStop);///////////////////////////////////
-    $('.bus').eq(indexInTheBuffer).click(function(){
-      if($('.bus').eq(indexInTheBuffer).hasClass('busToOBC')
-        && $('.bus').eq(indexInTheBuffer).hasClass('busBeforeDeparture')){
-        score+=1;
+  ids.push(setTimeout(function () { //ARRIVAL
+    var intervalBusApproach = setInterval(function () {
+      $('.bus').eq(indexInTheBuffer).css('margin-left', '+=2px');
+      distanceDone += 2;
+      if (distanceDone === initialMargin + distanceTarget) {
+        clearInterval(intervalBusApproach);
       }
-      else if($('.bus').eq(indexInTheBuffer).hasClass('busToNowhere')
+    }, speed);
+    ids.push(intervalBusApproach);
+  }, delayTime));
+
+  ids.push(setTimeout(function () {
+    $('.bus').eq(indexInTheBuffer).click(function () {
+      if ($('.bus').eq(indexInTheBuffer).hasClass('busToOBC')
+        && $('.bus').eq(indexInTheBuffer).hasClass('busBeforeDeparture')) {
+        score += 1;
+      }
+      else if ($('.bus').eq(indexInTheBuffer).hasClass('busToNowhere')
         && $('.bus').eq(indexInTheBuffer).hasClass('busBeforeDeparture')) {
         score -= 10;
       }
       $('.actualScore').html(score);
     });
-  },delayTime+approachTime);
+  }, delayTime + approachTime));
 
-  var timeOutBusDeparture = setTimeout(function(){ // DEPARTURE
-    intervalsAndTimeouts.push(timeOutBusDeparture);///////////////////////////////
+  ids.push(setTimeout(function () { // DEPARTURE
     $('.bus').eq(indexInTheBuffer).removeClass('busBeforeDeparture');
-    var intervalBusDeparture = setInterval(function(){
-      intervalsAndTimeouts.push(intervalBusDeparture);////////////////////////////
-      $('.bus').eq(indexInTheBuffer).css('margin-left','+=2px');
-      distanceDone+=2;
-      if(distanceDone===1100){
+    var intervalBusDeparture = setInterval(function () {
+      $('.bus').eq(indexInTheBuffer).css('margin-left', '+=2px');
+      distanceDone += 2;
+      if (distanceDone === 1100) {
         clearInterval(intervalBusDeparture);
       }
-    },speed);
-  },delayTime+approachTime+stopTime);
+    }, speed);
+    ids.push(intervalBusDeparture);
+  }, delayTime + approachTime + stopTime));
 }
