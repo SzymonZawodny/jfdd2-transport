0a39f68c46d1a969c38b3e5ebd353e71a8d6333f
mateuszRybczonek <m.rybczonek@gmail.com>
2016-03-24 15:18:41 +0100

    - text added;

--- js/showHideMainFunctionsDescriptionPages.js
+++ js/showHideMainFunctionsDescriptionPages.js
@@ -1,9 +1,11 @@
 $(document).ready(function(){
   $('.function-description-page').addClass('invisible');
 });
+
 $('.main-functions-images').eq(0).click(function(){
-  $('.function-description-page').removeClass('invisible');
+  $('.function-description-page').eq(0).removeClass('invisible');
 });
+
 $('.function-description-page').click(function() {
   $('.function-description-page').addClass('invisible');
 });
