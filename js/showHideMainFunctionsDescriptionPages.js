83228395dc0e0bb0b317be962fa09cff2bdd1829
mateuszRybczonek <m.rybczonek@gmail.com>
2016-03-24 16:06:41 +0100

    - four pages created; - closing cross added;

--- js/showHideMainFunctionsDescriptionPages.js
+++ js/showHideMainFunctionsDescriptionPages.js
@@ -2,10 +2,25 @@ $(document).ready(function(){
   $('.function-description-page').addClass('invisible');
 });
 
-$('.main-functions-images').eq(0).click(function(){
+var $functionImages = $('.main-functions-images');
+
+$functionImages.eq(0).click(function(){
   $('.function-description-page').eq(0).removeClass('invisible');
 });
 
-$('.function-description-page').click(function() {
+$functionImages.eq(1).click(function(){
+  $('.function-description-page').eq(1).removeClass('invisible');
+});
+
+$functionImages.eq(2).click(function(){
+  $('.function-description-page').eq(2).removeClass('invisible');
+});
+
+$functionImages.eq(3).click(function(){
+  $('.function-description-page').eq(3).removeClass('invisible');
+});
+
+$('.function-description-page-cross').click(function() {
   $('.function-description-page').addClass('invisible');
 });
+
